e3afce3fc4415135510bc619256f987741ab8dcc
afrahao <15301043@bjtu.edu.cn>
2017-06-06 23:39:58 +0800

    Create ToLoginServlet.java

---
+++ ContractManagement/src/com/web/ToLoginServlet.java
@@ -0,0 +1,23 @@
+package com.web;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class ToLoginServlet extends HttpServlet {
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.getRequestDispatcher("/login.jsp")
+				.forward(request, response);
+	}
+}
