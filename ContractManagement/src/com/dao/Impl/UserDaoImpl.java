c245d30b57fee1146ffc9fa6f74e40541dd1c91f
TheWindRisesLwj <lwjwind@163.com>
2017-06-13 16:18:10 +0800

    Update UserDaoImpl.java

--- ContractManagement/src/com/dao/Impl/UserDaoImpl.java
+++ ContractManagement/src/com/dao/Impl/UserDaoImpl.java
@@ -1,14 +1,16 @@
-package com.ruanko.dao.impl;
+package com.dao.impl;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
-import com.ruanko.dao.UserDao;
-import com.ruanko.model.User;
-import com.ruanko.utils.AppException;
-import com.ruanko.utils.DBUtil;
+import com.dao.UserDao;
+import com.model.User;
+import com.utils.AppException;
+import com.utils.DBUtil;
 
 /**
  * User data access layer implementation class
@@ -16,14 +18,14 @@ import com.ruanko.utils.DBUtil;
 public class UserDaoImpl implements UserDao {
 
 	/**
-	 *  Verify whether exists users that have the same name
+	 *  Verify whether exist user that has the same name
 	 * 
 	 * @param name User name
 	 * @return  Return true if there are users with same name,otherwise return false 
 	 * @throws AppException
 	 */
 	public boolean isExist(String name) throws AppException {
-		Connection conn = null; // Define Connection object
+		Connection conn = null; // Define database connection object
 		PreparedStatement psmt = null;// Define PreparedStatement object
 		ResultSet rs = null;// Define ResultSet object
 
@@ -37,7 +39,7 @@ public class UserDaoImpl implements UserDao {
 			psmt.setString(1, name);// Set values for the placeholder 
 
 			rs = psmt.executeQuery();// Execute the query, return the query results
-			if (rs.next()) {// Determine whether there are values in ResultSet
+			if (rs.next()) {// Determine whether there are values in results set
 				flag = true;
 			}
 		} catch (SQLException e) {
@@ -45,9 +47,9 @@ public class UserDaoImpl implements UserDao {
 			throw new AppException(
 					"com.ruanko.dao.impl.UserDaoImpl.isExist");
 		} finally {
-			DBUtil.closeResultSet(rs);// Close ResultSet object
-			DBUtil.closeStatement(psmt);//  Close PreparedStatement object 
-			DBUtil.closeConnection(conn);// Close Connection object
+			DBUtil.closeResultSet(rs);// Close database query result set
+			DBUtil.closeStatement(psmt);//  Close database object pretreatment
+			DBUtil.closeConnection(conn);// Close database connection object
 		}
 		return flag;
 	}
@@ -60,7 +62,7 @@ public class UserDaoImpl implements UserDao {
 	 * @throws AppException
 	 */
 	public boolean add(User user) throws AppException {
-		Connection conn = null; // Define Connection object
+		Connection conn = null; // Define database connection object
 		PreparedStatement psmt = null;// Define PreparedStatement object
 		
 		boolean flag = false;// Operation flag
@@ -84,23 +86,23 @@ public class UserDaoImpl implements UserDao {
 			e.printStackTrace();
 			throw new AppException("com.ruanko.dao.impl.UserDaoImpl.add");
 		} finally {
-			DBUtil.closeStatement(psmt);//  Close PreparedStatement object 
-			DBUtil.closeConnection(conn);// Close Connection obje
+			DBUtil.closeStatement(psmt);//  Close database object pretreatment
+			DBUtil.closeConnection(conn);// Close database connection object
 		}
 		return flag;
 	}
 	
 	/**
-	 * Query user id according to the user name and password
+	 * Query user number according to the user name and password
 	 * 
 	 * @param name 
 	 * @param password 
-	 * @return User id
+	 * @return User number
 	 * @throws AppException
 	 */
 	public int login(String name, String password) throws AppException {
-		int userId = -1; // Initialize the user id
-		//Declare Connection object,PreparedStatement object and ResultSet object
+		int userId = -1; // Initialize userId
+		//Declare database connection object, pre-compiled object and result set object
 		Connection conn = null;
 		PreparedStatement psmt = null;
 		ResultSet rs = null;
@@ -110,14 +112,14 @@ public class UserDaoImpl implements UserDao {
 			conn = DBUtil.getConnection();
 			// Declare operation statement:query user id according to the user name and password , "?" is a placeholder
 			String sql = "select id from t_user where name = ? and password = ? and del = 0";
-			// pre-compiled sql
+			//  pre-compiled sql
 			psmt = conn.prepareStatement(sql);
 			// Set values for the placeholder
 			psmt.setString(1, name);
 			psmt.setString(2, password);
 			// Execute the query operation
 			rs = psmt.executeQuery();
-			// Query record and extract the user id
+			// Query record and get  user id
 			if (rs.next()) {
 				userId = rs.getInt("id");
 			}
@@ -126,11 +128,101 @@ public class UserDaoImpl implements UserDao {
 			e.printStackTrace();
 			throw new AppException("com.ruanko.dao.impl.UserDaoImpl.login");
 		} finally {
-			// Close the database operation object, release resources
+			// Close database object operation, release resources
 			DBUtil.closeResultSet(rs);
 			DBUtil.closeStatement(psmt);
 			DBUtil.closeConnection(conn);
 		}
 		return userId;
 	}
+	
+	/**
+	 * Query user information according to id
+	 * 
+	 * @param id User id
+	 * @return User User object
+	 * @throws AppException
+	 */
+	public User getById(int id) throws AppException {
+		// Declare user object
+		User user = null;
+		//Declare database connection object, pre-compiled object and result set object
+		Connection conn = null;
+		PreparedStatement psmt = null;
+		ResultSet rs = null;
+		try {
+			// Create database connection
+			conn = DBUtil.getConnection();
+			// Declare operation statement:query user information according to the user id , "?" is a placeholder
+			String sql = "select id,name,password "
+					+"from t_user "
+					+"where id = ? and del = 0";
+			// pre-compiled sql
+			psmt = conn.prepareStatement(sql);
+			// Set values for the placeholder
+			psmt.setInt(1, id);
+			// Query resultSet
+			rs = psmt.executeQuery();
+			
+			// Save user information in Pole entity object when queried out resultSet
+			if (rs.next()) {
+				user = new User(); // Instantiate user objects
+				// Set value to user object
+				user.setId(rs.getInt("id"));
+				user.setName(rs.getString("name"));
+				user.setPassword(rs.getString("password"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new AppException("com.ruanko.dao.impl.UserDaoImpl.getById");
+		} finally {
+			// Close database object operation, release resources
+			DBUtil.closeResultSet(rs);
+			DBUtil.closeStatement(psmt);
+			DBUtil.closeConnection(conn);
+		}
+		return user;
+	}
+
+	/**
+	 * Query user id set
+	 * 
+	 * @return User id set
+	 * @throws AppException
+	 */
+	public List<Integer> getIds() throws AppException {
+		// Initialiaze ids
+		List<Integer> ids = new ArrayList<Integer>();
+		
+		//Declare Connection object,PreparedStatement object and ResultSet object
+		Connection conn = null;
+		PreparedStatement psmt = null;
+		ResultSet rs = null;
+		
+		try {
+			// Create database connection
+			conn = DBUtil.getConnection();
+			// Declare operation statement:query user id set,"?" is a placeholder
+			String sql = "select id from t_user where del = 0";
+			
+			psmt = conn.prepareStatement(sql);
+			
+			rs = psmt.executeQuery();// Return result set
+			// Loop to get information in result set,and save in ids
+			while (rs.next()) {
+				ids.add(rs.getInt("id"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new AppException(
+					"com.ruanko.dao.impl.UserDaoImpl.getIds");
+		} finally {
+			// Close database operation object, release resources
+			DBUtil.closeResultSet(rs);
+			DBUtil.closeStatement(psmt);
+			DBUtil.closeConnection(conn);
+		}
+		return ids;
+	}
+
 }
