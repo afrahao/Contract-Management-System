41fcc5ac53d94b40ddfc91e98cffe6c981747bfd
afrahao <15301043@bjtu.edu.cn>
2017-06-13 18:09:30 +0800

    Update UserDao.java

--- ContractManagement/src/com/dao/UserDao.java
+++ ContractManagement/src/com/dao/UserDao.java
@@ -3,28 +3,62 @@ package com.dao;
 import java.util.List;
 
 import com.model.User;
+import com.utils.AppException;
 
+/**
+ * User Data Access Layer Interface
+ */
 public interface UserDao {
-	public boolean isExist(String name);
 	
-	/*
-	  Save user's information
+	/**
+	 * Verify whether exist user that  has the same name 
+	 * 
+	 * @param name  User name
+	 * @return Return true if exist user that  has the same name ，otherwise false
+	 * @throws AppException
 	 */
-	public boolean add(User user);
+	public boolean isExist(String name) throws AppException;
+	public boolean isExist(int id) throws AppException;
 	
-	/*
-	  Query  UserId according to user name and password
+	/**
+	 * Save user's information
+	 * 
+	 * @param user User object
+	 * @return Return true if save successfully, otherwise false
+	 * @throws AppException
 	 */
-	public int login(String name,String password);
+	public boolean add(User user) throws AppException;
 	
-	/*
-	  Query user's information according to id
-	 */
-	public User getById(int id);
+	public boolean delete(int id) throws AppException;
 	
-	/*
-	  Query user id set
+	/**
+	 * Query  UserId according to user name and password
+	 * @param name User name
+	 * @param password 
+	 * @throws AppException
 	 */
-	public List<Integer> getIds();
+	public int login(String name,String password) throws AppException;
+	
+	/**
+	 * Query user's information according to id
+	 * 
+	 * @param id  User id
+	 * @return User 
+	 * @throws AppException
+	 */
+	public User getById(int id) throws AppException;
+	
+	/**
+	 * Query user id set
+	 * 
+	 * @return User id set
+	 * @throws AppException
+	 */
+	public List<Integer> getIds() throws AppException;
+	
+	public boolean modify(int id, String password) throws AppException;
+	public boolean isExistUser(String name) throws AppException;
+	public boolean isSame(String name, String oPassword) throws AppException;
+	public boolean ChangePassword(String name, String nPassword) throws AppException;
 	
 }
