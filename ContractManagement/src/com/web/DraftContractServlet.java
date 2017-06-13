562017c79b4ed65692e1386cf0c9311b48f10a9d
TheWindRisesLwj <lwjwind@163.com>
2017-06-13 16:43:20 +0800

    Create DraftContractServlet.java

---
+++ ContractManagement/src/com/web/DraftContractServlet.java
@@ -0,0 +1,129 @@
+package com.web;
+
+import java.io.IOException;
+import java.rmi.AccessException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.dao.impl.CustomerDaoImpl;
+import com.model.Contract;
+import com.model.Customer;
+import com.service.ContractService;
+import com.utils.AppException;
+
+
+/**
+ * Servlet for draft contract
+ */
+public class DraftContractServlet extends HttpServlet {
+
+	/**
+	 * Process POST requests of draft contract
+	 */
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		//Set the request's character encoding
+		request.setCharacterEncoding("UTF-8");
+		
+		// Declare session
+		HttpSession session = null;
+		// Get session by using request object
+		session = request.getSession();
+		Integer userId = (Integer)session.getAttribute("userId");
+		 
+		// If user is not login, jump to login page
+		if (userId == null) {
+			response.sendRedirect("toLogin");
+		}else {
+			// Get contract data information
+			String name = request.getParameter("name");
+			String customer = request.getParameter("customer");
+			String content = request.getParameter("content");
+			String beginTime = request.getParameter("beginTime");
+			String endTime = request.getParameter("endTime");
+			CustomerDaoImpl customerdao=new CustomerDaoImpl();
+			try {
+				if(customerdao.isExist(name)==false)
+				{
+					Customer cust=new Customer();
+					cust.setAccout("");
+					cust.setAddress("");
+					cust.setBank("");
+					cust.setCode("");
+					cust.setDel(0);
+					cust.setFax("");
+					cust.setName(customer);
+					cust.setNum("");
+					cust.setTel("");
+					customerdao.addCustomer(cust);
+				}
+			} catch (AppException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			// Instantiate begin and end of java.util.Date type,for accepting transformed beginTime and endTime
+			Date begin = new Date();
+			Date end = new Date();
+			
+			// Define a date format object, transform the time of String type into java.util.Date data type 
+			SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		
+			// Initialize prompt message
+			String message = "";
+			
+			try {
+				begin = dateFormat.parse(beginTime);
+				end = dateFormat.parse(endTime);
+				
+				// Build a Contract object and assign value for the object's attribute
+				Contract contract = new Contract();
+				contract.setName(name);
+				contract.setCustomer(customer);
+				contract.setBeginTime(begin);
+				contract.setEndTime(end);
+				contract.setContent(content);
+				contract.setUserId(userId);
+				
+				// Initialize contractService
+				ContractService contractService = new ContractService();
+				
+				// Operation success or failure, return draft page, giving prompt message
+				if (contractService.draft(contract)) {
+					message = "Drafting succeeded!";
+					// Transform the information created now to page for display
+					request.setAttribute("contract", contract);
+				} else {
+					message = "Drafting failure!";
+				}
+				
+			} catch (ParseException e) {
+				e.printStackTrace();
+				message = "Contract data is required. Incorrect date format";
+			} catch (AppException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			// Save message to request
+			request.setAttribute("message", message);
+			// Forward to draft page 
+			request.getRequestDispatcher("/DraftContract.jsp").forward(request, response);
+		}
+	}
+	
+	/**
+	 * Process the GET requests
+	 */
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// Call doPost() to process request
+		doPost(request,response);
+	}
+
+}
