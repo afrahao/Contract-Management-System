e8d66c2938a4d858d9f488a16e805c0c1f2c2357
afrahao <15301043@bjtu.edu.cn>
2017-06-21 00:04:13 +0800

    Update DraftContractServlet.java

--- ContractManagement/src/com/web/DraftContractServlet.java
+++ ContractManagement/src/com/web/DraftContractServlet.java
@@ -1,96 +1,47 @@
 package com.web;
 
-import java.io.IOException;
-import java.rmi.AccessException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.io.*;
+import java.text.*;
+import java.util.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import com.model.*;
+import com.service.*;
+import com.utils.*;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-<<<<<<< HEAD
-import com.model.Contract;
-=======
-import com.dao.impl.CustomerDaoImpl;
-import com.model.Contract;
-import com.model.Customer;
->>>>>>> origin/LiWenjie
-import com.service.ContractService;
-import com.utils.AppException;
-
-
-/**
- * Servlet for draft contract
- */
 public class DraftContractServlet extends HttpServlet {
 
-	/**
-	 * Process POST requests of draft contract
-	 */
-	public void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		//Set the request's character encoding
+	public void doPost(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException
+	{
 		request.setCharacterEncoding("UTF-8");
-		
-		// Declare session
 		HttpSession session = null;
-		// Get session by using request object
 		session = request.getSession();
+		//get user id
 		Integer userId = (Integer)session.getAttribute("userId");
 		 
-		// If user is not login, jump to login page
-		if (userId == null) {
+		//let user login if not
+		if (userId == null) 
+		{
 			response.sendRedirect("toLogin");
 		}else {
-			// Get contract data information
+			// get contract information
 			String name = request.getParameter("name");
 			String customer = request.getParameter("customer");
 			String content = request.getParameter("content");
 			String beginTime = request.getParameter("beginTime");
 			String endTime = request.getParameter("endTime");
-<<<<<<< HEAD
-			
-=======
-			CustomerDaoImpl customerdao=new CustomerDaoImpl();
-			try {
-				if(customerdao.isExist(name)==false)
-				{
-					Customer cust=new Customer();
-					cust.setAccout("");
-					cust.setAddress("");
-					cust.setBank("");
-					cust.setCode("");
-					cust.setDel(0);
-					cust.setFax("");
-					cust.setName(customer);
-					cust.setNum("");
-					cust.setTel("");
-					customerdao.addCustomer(cust);
-				}
-			} catch (AppException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-			}
->>>>>>> origin/LiWenjie
-			// Instantiate begin and end of java.util.Date type,for accepting transformed beginTime and endTime
+			String message = "";
 			Date begin = new Date();
 			Date end = new Date();
-			
-			// Define a date format object, transform the time of String type into java.util.Date data type 
 			SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-		
-			// Initialize prompt message
-			String message = "";
 			
-			try {
+			
+			try 
+			{
 				begin = dateFormat.parse(beginTime);
 				end = dateFormat.parse(endTime);
 				
-				// Build a Contract object and assign value for the object's attribute
+				//set contract information
 				Contract contract = new Contract();
 				contract.setName(name);
 				contract.setCustomer(customer);
@@ -99,42 +50,37 @@ public class DraftContractServlet extends HttpServlet {
 				contract.setContent(content);
 				contract.setUserId(userId);
 				
-				// Initialize contractService
 				ContractService contractService = new ContractService();
-<<<<<<< HEAD
-				ContractService contractServiceadmin = new ContractService();
-=======
->>>>>>> origin/LiWenjie
-				
-				// Operation success or failure, return draft page, giving prompt message
-				if (contractService.draft(contract)) {
+				//try to create the contract
+				if (contractService.draft(contract))
+				{
 					message = "Drafting succeeded!";
-					// Transform the information created now to page for display
+					//send the contract to jsp
 					request.setAttribute("contract", contract);
-				} else {
+				} 
+				else
+				{
 					message = "Drafting failure!";
 				}
-				
-			} catch (ParseException e) {
+			} catch (ParseException e) 
+			{
 				e.printStackTrace();
 				message = "Contract data is required. Incorrect date format";
-			} catch (AppException e) {
-				// TODO Auto-generated catch block
+			}
+			catch (AppException e) 
+			{
 				e.printStackTrace();
 			}
-			// Save message to request
+			//send message to jsp
 			request.setAttribute("message", message);
-			// Forward to draft page 
+			//turn to jsp 
 			request.getRequestDispatcher("/DraftContract.jsp").forward(request, response);
 		}
 	}
 	
-	/**
-	 * Process the GET requests
-	 */
+	
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// Call doPost() to process request
 		doPost(request,response);
 	}
 
