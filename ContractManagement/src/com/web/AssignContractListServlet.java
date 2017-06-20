18bd06e8b399845fed322582a66e212a8bdde18e
afrahao <15301043@bjtu.edu.cn>
2017-06-20 23:37:09 +0800

    Update AssignContractListServlet.java

--- ContractManagement/src/com/web/AssignContractListServlet.java
+++ ContractManagement/src/com/web/AssignContractListServlet.java
@@ -1,115 +1,92 @@
 package com.web;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.*;
+import java.util.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import com.model.*;
+import com.service.*;
+import com.utils.*;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
-import com.model.ConBusiModel;
-import com.model.Role;
-import com.service.ContractService;
-import com.service.UserService;
-import com.utils.AppException;
-
-/**
- * Servlet for accessing page of contract to be distributed 
- */
 public class AssignContractListServlet extends HttpServlet{
 
-	/**
-	 * Jump to page of contract to be distributed 
-	 */
-	public void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {	
-		// Set output content's type
+	public void doPost(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException
+	{	
 		response.setContentType("text/html");
-		// Set the response's character encoding
-		response.setCharacterEncoding("UTF-8");
-		// Set the request's character encoding
 		request.setCharacterEncoding("UTF-8");
+		response.setCharacterEncoding("UTF-8");
 		
-		// Declare session
-		HttpSession session = null;
-		// Get session by using request
-		session = request.getSession();
+		HttpSession session = request.getSession();
 		Integer userId = (Integer)session.getAttribute("userId");
 		
-		// If user is not login, jump to login page
-		if (userId == null) {
+		//everyone need to login
+		if (userId == null)
+		{
 			response.sendRedirect("toLogin");
-		}else {
+		}
+		else
+		{
 			try {
-				// Initialize userService
 				UserService userService = new UserService();
-				// Call business logic layer to get information corresponding to the user's role
 				Role role = userService.getUserRole(userId);
 				
-				// Process page jump according to user's role
-				if ( role == null) {
-					//  Redirect to new user page
+				//if the user has no privilege
+				if ( role == null) 
+				{
 					response.sendRedirect("toNewUser");
-				}else {
-					//  Get function id string
-					String funcIds = role.getFuncIds();
-					// Define opration flag,respectively identify whether have "assign countersign", "assign approval", "assign sign" permission
-					boolean flag1 = false;
-					boolean flag2 = false;
-					boolean flag3 = false;
+				}
+				else
+				{
+					//get functions the user can process
+					String funcIds = role.getFuncIds();	
+					boolean countersign = false,approve = false,sign = false;
 					
-					
-					// Separate the function id string with comma to be string array,and traverse it
 					for (String id : funcIds.split(",")) {
-						if (id.equals("008")) {// Have "assign countersign" permission
-							flag1 = true;
+						// assign countersign
+						if (id.equals("008"))
+						{
+							countersign = true;
 						}
-						if (id.equals("009")) {//  Have "assign approval" permission
-							flag2 = true;
+						//assign approval
+						if (id.equals("009"))
+						{
+							approve = true;
 						}
-						if (id.equals("010")) {// Have "assign sign" permission
-							flag3 = true;
+						//assign sign
+						if (id.equals("010"))
+						{
+							sign = true;
 						}
 					}
 					
-					// Only when have the three permissions can do the operation to assign contract
-					if (flag1 && flag2 && flag3) {
-						// Initialize contractService
+					//the user can assign all the privilege
+					if (countersign && approve && sign) {
 						ContractService contractService = new ContractService();
-						// Initialize contractList
 						List<ConBusiModel> contractList = new ArrayList<ConBusiModel>();
-						// Call business logic layer to get list of contract to be distributed 
-						contractList = contractService.getDfphtList();
-						// Save contractList to request
+						contractList = contractService.getAssignList();
+						//send the assign contract list to jsp
 						request.setAttribute("contractList", contractList);
-						// Forward to page of contract to be distributed
 						request.getRequestDispatcher("/AssignContractList.jsp").forward(request, response);
-					}else {// Otherwise can not do the operation to assign contract
-						
-						PrintWriter out = response.getWriter();// Get output object
+					}
+					else
+					{	
+						PrintWriter out = response.getWriter();
 						out.println("<script language = javascript>alert('You don't have the permission!')");
-						out.println("window.history.go(-1)</script>");
 						response.sendRedirect("toNewUser");
 					}
 				}
-			} catch (AppException e) {
+			}
+			catch (AppException e) 
+			{
 				e.printStackTrace();
-				//Redirect to the exception page
 				response.sendRedirect("toError");
 			}
 		}
 	}
 	
-	/**
-	 * Process GET requests
-	 */
-	public void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		// Call doPost() to process request
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
 		this.doPost(request, response);
 	}
 
