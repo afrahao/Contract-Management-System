200c6e7fa86826be44946f6e3cabd87dbd05eee7
afrahao <15301043@bjtu.edu.cn>
2017-06-20 23:53:47 +0800

    Update ConfigurePermissionServlet.java

--- ContractManagement/src/com/web/ConfigurePermissionServlet.java
+++ ContractManagement/src/com/web/ConfigurePermissionServlet.java
@@ -1,75 +1,52 @@
 package com.web;
 
-import java.io.IOException;
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import com.model.*;
+import com.service.*;
+import com.utils.*;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-import com.model.Right;
-import com.service.UserService;
-import com.utils.AppException;
-
-/**
- * Servlet for configuring Permission
- */
 public class ConfigurePermissionServlet extends HttpServlet {
 
-	/**
-	 * Process request of configuring Permission
-	 */
 	public void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// Set the request's character encoding
 		request.setCharacterEncoding("UTF-8");
-
-		// Declare session
 		HttpSession session = null;
-		// Get session by using request
 		session = request.getSession();
 		Integer userId = (Integer) session.getAttribute("userId");
 
-		// If user is not login, jump to login page
-		if (userId == null) {
+		//let the user login if not
+		if (userId == null)
+		{
 			response.sendRedirect("toLogin");
-		} else {
-
-			/*
-			 * Get Permission configuration information
-			 */
-			// Get user id that configured permission
+		} 
+		else
+		{
+			//get id and role id
 			int uId = Integer.parseInt(request.getParameter("userId").toString());
-			// Get roleId
 			int roleId = Integer.parseInt(request.getParameter("roleId").toString());
-			// Create a right object,and assign value to it
 			Right right = new Right();
 			right.setUserId(uId);
 			right.setRoleId(roleId);
 			
-			try {
-				// Initialize userService
+			try 
+			{
 				UserService userService = new UserService();
-				// Call business logic layer to configure permission
 				userService.assignPermission(right);
-				
-				// After configuration,redirect to permission configuration page
+				//show the permission list to jsp
 				response.sendRedirect("configurePermissionList");
-			} catch (AppException e) {
+			}
+			catch (AppException e)
+			{
 				e.printStackTrace();
-				// Redirect to the exception page
 				response.sendRedirect("toError");
 			}
 		}
 	}
 
-	/**
-	 * Process GET requests
-	 */
-	public void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		// Call doPost() to process request
+	public void doGet(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException 
+	{
 		this.doPost(request, response);
 	}
 
