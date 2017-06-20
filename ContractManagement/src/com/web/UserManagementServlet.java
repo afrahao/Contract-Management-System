a2b8aa3378c7c23b3bb7079cb1c1c93fcda24dad
TheWindRisesLwj <lwjwind@163.com>
2017-06-21 02:36:27 +0800

    Update UserManagementServlet.java

--- ContractManagement/src/com/web/UserManagementServlet.java
+++ ContractManagement/src/com/web/UserManagementServlet.java
@@ -1,47 +1,32 @@
 package com.web;
 
-import java.io.IOException;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-/**
- * Servlet for accessing administrator page
- */
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
 public class UserManagementServlet extends HttpServlet {
 
-	/**
-	 * Jump to administrator page
-	 */
 	public void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// Set the request's character encoding
 		request.setCharacterEncoding("UTF-8");
-		
-		// Declare session
 		HttpSession session = null;
-		// Get session by using request object
 		session = request.getSession();
 		Integer userId = (Integer)session.getAttribute("userId");
 		
 		// If the user is not login, then jump to login page
-		if (userId == null) {
+		if (userId == null) 
+		{
 			response.sendRedirect("toLogin");
-		}else {
-			// Forwarded to contract administrator page
+		}
+		else
+		{
+			// Forwarded to user management page
 			request.getRequestDispatcher("/UserManagement.jsp").forward(request, response);
 		}
 	}
 
-	/**
-	 * Process GET requests
-	 */
+
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// Call doPost() to process request
 		this.doPost(request, response);
 	}
 	
