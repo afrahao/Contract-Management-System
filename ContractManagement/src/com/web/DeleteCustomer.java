7f856a1514bb68ca189ac1f33428729f02cbd715
afrahao <15301043@bjtu.edu.cn>
2017-06-21 00:02:49 +0800

    Update DeleteCustomer.java

--- ContractManagement/src/com/web/DeleteCustomer.java
+++ ContractManagement/src/com/web/DeleteCustomer.java
@@ -1,49 +1,36 @@
 package com.web;
 
-import java.io.IOException;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
 import com.dao.impl.CustomerDaoImpl;
-import com.dao.impl.RoleDaoImpl;
-import com.model.Customer;
-import com.model.Role;
 
 public class DeleteCustomer extends HttpServlet{
 
-	/**
-	 * Jump to page of user permission list
-	 */
-	public void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {	
-		// Set character set of request to "UTF-8"
+	public void doPost(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException
+	{
 		request.setCharacterEncoding("UTF-8");
-		
-		// Declare session
-		HttpSession session = null;
-		// Get session by using request
-		session = request.getSession();
+		HttpSession session = request.getSession();
+		//get user id
 		Integer userId = (Integer)session.getAttribute("userId");
 		CustomerDaoImpl customerdao=new CustomerDaoImpl();
-		// If user is not login, jump to login page
-		if (userId == null) {
+		//let user login if not
+		if (userId == null) 
+		{
 			response.sendRedirect("toLogin");
-		}else {
+		}
+		else
+		{
+			//delete customer
 			customerdao.deleteCustomer(Integer.parseInt(request.getParameter("conId")));
+			//turn to jsp
 			response.sendRedirect("customerList.jsp");
 		}
 	}
 	
-	/**
-	 * Process GET requests
-	 */
-	public void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		// Call doPost() to process request
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException 
+	{
 		this.doPost(request, response);
 	}
 
