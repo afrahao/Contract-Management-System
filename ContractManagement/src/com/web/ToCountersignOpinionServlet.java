133ff5ef82a75b91cf33ef1b266b8c2a95dc4bee
afrahao <15301043@bjtu.edu.cn>
2017-06-21 00:34:40 +0800

    Update ToCountersignOpinionServlet.java

--- ContractManagement/src/com/web/ToCountersignOpinionServlet.java
+++ ContractManagement/src/com/web/ToCountersignOpinionServlet.java
@@ -1,69 +1,51 @@
 package com.web;
 
-import java.io.IOException;
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import com.model.*;
+import com.service.*;
+import com.utils.*;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-import com.model.Contract;
-import com.service.ContractService;
-import com.utils.AppException;
-
-/**
- * Servlet for accessing countersign page
- */
 public class ToCountersignOpinionServlet extends HttpServlet {
 
-	/**
-	 * Jump to countersign page
-	 */
 	public void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// Set the request's character encoding
 		request.setCharacterEncoding("UTF-8");
-		
-		// Declare session
 		HttpSession session = null;
-		// Get session by using request
 		session = request.getSession();
+		//get user id
 		Integer userId = (Integer)session.getAttribute("userId");
 		
-		// If user is not login, jump to login page
-		if (userId == null) {
+		//let user login if it didn't login
+		if (userId == null) 
+		{
 			response.sendRedirect("toLogin");
-		} else {
+		}
+		else 
+		{
 
 			// Get contract id
 			int conId = Integer.parseInt(request.getParameter("conId"));
 
 			try {
-				// Initialize contractService
 				ContractService contractService = new ContractService();
-				// Query contract information according to contract id
+				//get contract
 				Contract contract = contractService.getContract(conId);
-
-				// Save contract to request
+				// seng contract to jsp
 				request.setAttribute("contract", contract);
-				//  Forward to countersign page
+				//  turn to page countersign
 				request.getRequestDispatcher("/CountersignOpinion.jsp").forward(
 						request, response);
 			} catch (AppException e) {
 				e.printStackTrace();
-				// Redirect to the exception page
 				response.sendRedirect("toError");
 			}
 		}
 	}
 
-	/**
-	 * Process GET requests
-	 */
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// Call doPost() to process request
 		this.doPost(request, response);
 	}
 }
