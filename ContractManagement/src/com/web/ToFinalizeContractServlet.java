d3a175359e0a7e80112d867cd63c37bd63303745
afrahao <15301043@bjtu.edu.cn>
2017-06-21 00:38:38 +0800

    Update ToFinalizeContractServlet.java

--- ContractManagement/src/com/web/ToFinalizeContractServlet.java
+++ ContractManagement/src/com/web/ToFinalizeContractServlet.java
@@ -1,38 +1,25 @@
 package com.web;
 
-import java.io.IOException;
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import com.model.*;
+import com.service.*;
+import com.utils.*;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-import com.model.Contract;
-import com.service.ContractService;
-import com.utils.AppException;
-
-/**
- * Access finalized contract page
- */
 public class ToFinalizeContractServlet extends HttpServlet{
 
-	/**
-	 * Jump to finalized contract page
-	 */
 	public void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {	
-		// Set the request's character encoding
 		request.setCharacterEncoding("UTF-8");
-		
-		// Declare session
 		HttpSession session = null;
-		//  Get session by using request
 		session = request.getSession();
+		//get user id
 		Integer userId = (Integer)session.getAttribute("userId");
 		
-		// If user is not login, jump to login page
-		if (userId == null) {
+		// let user login
+		if (userId == null)
+		{
 			response.sendRedirect("toLogin");
 		}else {
 			
@@ -40,30 +27,26 @@ public class ToFinalizeContractServlet extends HttpServlet{
 			int conId = Integer.parseInt(request.getParameter("conId"));
 
 			try {
-				// Initialize  contractService
 				ContractService contractService = new ContractService();
-				//Query contract information according to Contract id
+				//get contract
 				Contract contract = contractService.getContract(conId);
 
-				// Save contract to request
+				// send contract to jsp
 				request.setAttribute("contract", contract);
-				// Forward to page of contract to be finalized 
+				// turn to page finalize contract
 				request.getRequestDispatcher("/FinalizeContract.jsp").forward(
 						request, response);
-			} catch (AppException e) {
+			}
+			catch (AppException e) 
+			{
 				e.printStackTrace();
-				// Redirect to the exception page
 				response.sendRedirect("toError");
 			}
 		}
 	}
 	
-	/**
-	 * Process GET requests
-	 */
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// Call doPost() to process request
 		this.doPost(request, response);
 	}
 
