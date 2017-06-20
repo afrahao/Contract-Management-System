65c4f5836cdbe8530d15cd637c70f63a3ddfba1e
TheWindRisesLwj <lwjwind@163.com>
2017-06-21 02:32:42 +0800

    Update ToQProcess.java

--- ContractManagement/src/com/web/ToQProcess.java
+++ ContractManagement/src/com/web/ToQProcess.java
@@ -1,40 +1,22 @@
 package com.web;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.*;
+import java.util.*;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+import javax.servlet.*;
+import javax.servlet.http.*;
 
 import com.dao.impl.ConStateDaoImpl;
-import com.dao.impl.RoleDaoImpl;
-import com.model.ConState;
-import com.model.PermissionBusiModel;
-import com.model.Role;
-import com.service.UserService;
-import com.utils.AppException;
-
-/**
- * Access page of user permission list
- */
+import com.model.*;
+import com.utils.*;
 public class ToQProcess extends HttpServlet{
 
-	/**
-	 * Jump to page of user permission list
-	 */
 	public void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {	
-		// Set character set of request to "UTF-8"
 		request.setCharacterEncoding("UTF-8");
-		
-		// Declare session
 		HttpSession session = null;
-		// Get session by using request
 		session = request.getSession();
+		//get user id
 		Integer userId = (Integer)session.getAttribute("userId");
 		
 		// If user is not login, jump to login page
@@ -47,22 +29,19 @@ public class ToQProcess extends HttpServlet{
 				List<ConState> constate=null;
 				constate=statedao.getAll();
 				request.setAttribute("constate", constate);
-			} catch (AppException e) {
-				// TODO Auto-generated catch block
+			}
+			catch (AppException e) 
+			{
 				e.printStackTrace();
 			}
-			// Forward to permission configuration page
+			// Forward to QProcess page
 			request.getRequestDispatcher("/QProcess.jsp").forward(
 					request, response);
 		}
 	}
-	
-	/**
-	 * Process GET requests
-	 */
+
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// Call doPost() to process request
 		this.doPost(request, response);
 	}
 
