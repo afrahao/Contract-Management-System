825ecc2a1cb6452789f52d7fa9307af1f18181b2
afrahao <15301043@bjtu.edu.cn>
2017-06-21 00:28:03 +0800

    Update ToAddRole.java

--- ContractManagement/src/com/web/ToAddRole.java
+++ ContractManagement/src/com/web/ToAddRole.java
@@ -1,57 +1,34 @@
 package com.web;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-import com.dao.impl.RoleDaoImpl;
-import com.model.PermissionBusiModel;
-import com.model.Role;
-import com.service.UserService;
-import com.utils.AppException;
-
-/**
- * Access page of user permission list
- */
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
 public class ToAddRole extends HttpServlet{
 
-	/**
-	 * Jump to page of user permission list
-	 */
-	public void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {	
-		// Set character set of request to "UTF-8"
+	public void doPost(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException 
+	{	
 		request.setCharacterEncoding("UTF-8");
-		
-		// Declare session
 		HttpSession session = null;
-		// Get session by using request
 		session = request.getSession();
+		//get user id
 		Integer userId = (Integer)session.getAttribute("userId");
 		
-		// If user is not login, jump to login page
-		if (userId == null) {
+		// remenber login!!!
+		if (userId == null) 
+		{
 			response.sendRedirect("toLogin");
-		}else {
+		}
+		else
+		{
 			
-			// Forward to permission configuration page
+			// turn to page permission configuration
 			request.getRequestDispatcher("/AddRole.jsp").forward(
 					request, response);
 		}
 	}
 	
-	/**
-	 * Process GET requests
-	 */
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// Call doPost() to process request
 		this.doPost(request, response);
 	}
 
