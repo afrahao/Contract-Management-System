19f7ea53b6f177dd9c01de3fb058f31ab6d86676
afrahao <15301043@bjtu.edu.cn>
2017-06-21 00:27:03 +0800

    Update ToAddCustomer.java

--- ContractManagement/src/com/web/ToAddCustomer.java
+++ ContractManagement/src/com/web/ToAddCustomer.java
@@ -1,57 +1,35 @@
 package com.web;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-import com.dao.impl.RoleDaoImpl;
-import com.model.PermissionBusiModel;
-import com.model.Role;
-import com.service.UserService;
-import com.utils.AppException;
-
-/**
- * Access page of user permission list
- */
 public class ToAddCustomer extends HttpServlet{
-
-	/**
-	 * Jump to page of user permission list
-	 */
+	
 	public void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {	
-		// Set character set of request to "UTF-8"
+			throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
-		
-		// Declare session
 		HttpSession session = null;
-		// Get session by using request
 		session = request.getSession();
+		//get user id
 		Integer userId = (Integer)session.getAttribute("userId");
 		
-		// If user is not login, jump to login page
-		if (userId == null) {
+		// I hate someone that not login....let the user login
+		if (userId == null) 
+		{
 			response.sendRedirect("toLogin");
-		}else {
+		}
+		else
+		{
 			
-			// Forward to permission configuration page
+			//turn to page 'addcustomer'
 			request.getRequestDispatcher("/AddCustomer.jsp").forward(
 					request, response);
 		}
 	}
 	
-	/**
-	 * Process GET requests
-	 */
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// Call doPost() to process request
 		this.doPost(request, response);
 	}
 
