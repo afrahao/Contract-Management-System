cc06feb537d9d3f6e1d68cfff98ba6d8a1e55fb1
TheWindRisesLwj <lwjwind@163.com>
2017-06-21 02:30:27 +0800

    Update ToNewUserServlet.java

--- ContractManagement/src/com/web/ToNewUserServlet.java
+++ ContractManagement/src/com/web/ToNewUserServlet.java
@@ -1,47 +1,30 @@
 package com.web;
 
-import java.io.IOException;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-/**
- * Servlet for accessing new user page 
- */
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
 public class ToNewUserServlet extends HttpServlet{
 	
-	/**
-	 * Jump to new user page
-	 */
 	public void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// SSet request's character encoding
 		request.setCharacterEncoding("UTF-8");
-		
-		// Declare session
 		HttpSession session = null;
-		// Get session by using request object
 		session = request.getSession();
+		//get user id
 		Integer userId = (Integer)session.getAttribute("userId");
 		
-		// If user is not login,jump to login page
-		if (userId == null) {
+		// let user login
+		if (userId == null)
+		{
 			response.sendRedirect("toLogin");
 		}else {
-			// Forward to the user page
+			// turn to the user page
 			request.getRequestDispatcher("/NewUser.jsp").forward(request, response);
 		}
 	}
 
-	/**
-	 * Process GET requests
-	 */
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// Call doPost() to process request
 		this.doPost(request, response);
 	}
 
