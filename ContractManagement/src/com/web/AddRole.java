33e35bf447a427f37a20e8b9285a01b3f8844137
afrahao <15301043@bjtu.edu.cn>
2017-06-20 23:30:29 +0800

    Update AddRole.java

--- ContractManagement/src/com/web/AddRole.java
+++ ContractManagement/src/com/web/AddRole.java
@@ -1,45 +1,37 @@
 package com.web;
 
 import java.io.IOException;
-
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-import com.dao.impl.RoleDaoImpl;
-import com.model.Role;
+import javax.servlet.http.*;
+import com.dao.impl.*;
+import com.model.*;
 
 public class AddRole extends HttpServlet{
 
-	/**
-	 * Jump to page of user permission list
-	 */
-	public void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {	
-		// Set character set of request to "UTF-8"
+	public void doPost(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException
+	{	
 		request.setCharacterEncoding("UTF-8");
-		
-		// Declare session
 		HttpSession session = null;
-		// Get session by using request
 		session = request.getSession();
 		Integer userId = (Integer)session.getAttribute("userId");
 		
-		// If user is not login, jump to login page
-		if (userId == null) {
+		//let the user login if he or she not login
+		if (userId == null) 
+		{
 			response.sendRedirect("toLogin");
-		}else {
+		}
+		else 
+		{
 			String[] privlege=request.getParameterValues("check");
 			String function;
 			RoleDaoImpl roledao=new RoleDaoImpl();
 			Role role=new Role();
-			// Forward to permission configuration page
+			//set the role information
 			role.setName(request.getParameter("name"));
 			role.setDescription(request.getParameter("content"));
 			role.setDel(0);
 			function=privlege[0];
+			//set the privilege
 			for(int i=1;i<privlege.length;i++)
 			{
 				function=function+","+privlege[i];
@@ -50,12 +42,8 @@ public class AddRole extends HttpServlet{
 		}
 	}
 	
-	/**
-	 * Process GET requests
-	 */
-	public void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		// Call doPost() to process request
+	public void doGet(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException 
+	{
 		this.doPost(request, response);
 	}
 
