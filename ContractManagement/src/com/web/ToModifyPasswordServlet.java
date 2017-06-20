424f002f04e948088d7a8b6c61bafd260c3624ff
TheWindRisesLwj <lwjwind@163.com>
2017-06-21 02:30:00 +0800

    Update ToModifyPasswordServlet.java

--- ContractManagement/src/com/web/ToModifyPasswordServlet.java
+++ ContractManagement/src/com/web/ToModifyPasswordServlet.java
@@ -1,67 +1,41 @@
 package com.web;
 
-import java.io.IOException;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-
-
-import com.model.Contract;
-import com.model.User;
-import com.service.ContractService;
-import com.utils.AppException;
-
-/**
- * Servlet of accessing sign page
- */
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import com.model.*;
 public class ToModifyPasswordServlet extends HttpServlet {
 
-	/**
-	 * Jump to sign page
-	 */
 	public void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// Set the request's character encoding
 		request.setCharacterEncoding("UTF-8");
-		
-		// Declare session
 		HttpSession session = null;
-		// Get session by using request object
 		session = request.getSession();
+		//get user id
 		Integer userId = (Integer)session.getAttribute("userId");
 		
-		// If the user is not login, then jump to login page
-		if (userId == null) {
+		// let user login if not
+		if (userId == null) 
+		{
 			response.sendRedirect("toLogin");
 		} else {
 
-			// Get contract id
+			// Get user name and id
 			String name = request.getParameter("userName");
 			String Id = request.getParameter("userId");
 			User user = new User();
 			user.setName(name);
 			user.setId(Integer.parseInt(Id.toString()));
 			user.setDel(0);
-			
-			
-			// Save contract to request
 			request.setAttribute("user", user);
-			// Forward to sign page
+			// turn to modify password page
 			request.getRequestDispatcher("/ModifyPassword.jsp").forward(
 					request, response);
 		}
 	}
 
-	/**
-	 * Process GET requests
-	 */
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// Call doPost() to process request
 		this.doPost(request, response);
 	}
 }
