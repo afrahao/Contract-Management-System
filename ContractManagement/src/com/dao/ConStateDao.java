7d957dc67910701002e53247cb26b3fc51c1f82d
afrahao <15301043@bjtu.edu.cn>
2017-06-20 23:13:40 +0800

    Update ConStateDao.java

--- ContractManagement/src/com/dao/ConStateDao.java
+++ ContractManagement/src/com/dao/ConStateDao.java
@@ -5,53 +5,19 @@ import java.util.List;
 import com.model.ConState;
 import com.utils.AppException;
 
-/**
- * Contract State Data Access Layer Interface
- */
+
 public interface ConStateDao {
 
-	/**
-	 * Add contract operation state information
-	 * 
-	 * @param  conState Contract state object
-	 * @return boolean   Return true if successful , otherwise false
-	 * @throws AppException
-	 */
+	//add contract state
 	public boolean add(ConState conState) throws AppException;
 	
-	/**
-	 * Query contract id set that meet the conditions according to the contract type
-	 * 
-	 * @param type Operation type
-	 * @return Contract id set
-	 * @throws AppException
-	 */
+	//get contract ids by type
 	public List<Integer> getConIdsByType(int type) throws AppException;
 	
-	/**
-	 * Query contract state information according to contract id and type
-	 * 
-	 * @param conId Contract id
-	 * @param type Operation type
-	 * @return Contract state object
-	 * @throws AppException
-	 */
+	//get contract state
 	public ConState getConState(int conId, int type) throws AppException;
 	
-	/**
-	 * Judgement records in contract table  according to contract id and type,
-	 * Judgement though the statistics of the total eligible records,
-	 * If total number of records is greater than 0, the record exist, return true, otherwise the record does not exist, returns false
-	 * 
-	 * @param con_id Countract id
-	 * @param type Operation type
-<<<<<<< HEAD
-	 * @return boolean Exist return true，otherwise return false
-=======
-	 * @return boolean Exist return true锛宱therwise return false
->>>>>>> origin/LiWenjie
-	 * @throws AppException
-	 */
+	//jusge if there is the contract state
 	public boolean isExist(int con_id, int type) throws AppException;
 	
 }
