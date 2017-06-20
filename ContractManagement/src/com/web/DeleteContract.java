0f7f733a8114986b4c29b8277bb9659934cdd2c6
afrahao <15301043@bjtu.edu.cn>
2017-06-21 00:01:58 +0800

    Update DeleteContract.java

--- ContractManagement/src/com/web/DeleteContract.java
+++ ContractManagement/src/com/web/DeleteContract.java
@@ -1,50 +1,37 @@
 package com.web;
 
-import java.io.IOException;
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import com.dao.impl.*;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-import com.dao.impl.ContractDaoImpl;
-import com.dao.impl.CustomerDaoImpl;
-import com.dao.impl.RoleDaoImpl;
-import com.model.Customer;
-import com.model.Role;
 
 public class DeleteContract extends HttpServlet{
 
-	/**
-	 * Jump to page of user permission list
-	 */
-	public void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {	
-		// Set character set of request to "UTF-8"
+	public void doPost(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException
+	{
 		request.setCharacterEncoding("UTF-8");
-		
-		// Declare session
 		HttpSession session = null;
-		// Get session by using request
 		session = request.getSession();
+		//get user id
 		Integer userId = (Integer)session.getAttribute("userId");
 		ContractDaoImpl contractdao=new ContractDaoImpl();
-		// If user is not login, jump to login page
-		if (userId == null) {
+		//let the user login if not
+		if (userId == null)
+		{
 			response.sendRedirect("toLogin");
-		}else {
+		}
+		else
+		{
+			//delete the contract
 			contractdao.deleteContract((Integer.parseInt(request.getParameter("conId"))));
 			response.sendRedirect("toTerminateContract");
 		}
 	}
 	
-	/**
-	 * Process GET requests
-	 */
-	public void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		// Call doPost() to process request
+	
+	public void doGet(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException 
+	{
 		this.doPost(request, response);
 	}
 
