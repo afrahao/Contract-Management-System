1dd72f53cc094e3f865f786285386b4cb0f01089
afrahao <15301043@bjtu.edu.cn>
2017-06-21 00:09:15 +0800

    Update FinalizedContractServlet.java

--- ContractManagement/src/com/web/FinalizedContractServlet.java
+++ ContractManagement/src/com/web/FinalizedContractServlet.java
@@ -1,68 +1,50 @@
 package com.web;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.*;
+import java.util.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import com.model.*;
+import com.service.*;
+import com.utils.*;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-import com.model.ConBusiModel;
-import com.service.ContractService;
-import com.utils.AppException;
-
-/**
- * Access page of contract to be approved
- */
 public class FinalizedContractServlet extends HttpServlet{
 
-	/**
-	 * Jump to page of contract to be approved
-	 */
-	public void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {	
-		// Set the request's character encoding
+	public void doPost(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException 
+	{
 		request.setCharacterEncoding("UTF-8");
-		
-		// Declare session
 		HttpSession session = null;
-		// Get session by using request
 		session = request.getSession();
+		//get user id
 		Integer userId = (Integer)session.getAttribute("userId");
 		
-		// If user is not login, jump to login page
-		if (userId == null) {
+		//let user login if not
+		if (userId == null) 
+		{
 			response.sendRedirect("toLogin");
-		}else {
-			
-			try {
-				// Initialize contractService
+		}
+		else
+		{
+			try
+			{
 				ContractService contractService = new ContractService();
-				// Initialize contractList
 				List<ConBusiModel> contractList = new ArrayList<ConBusiModel>();
-				// Call business logic layer to get list of contract to be approved
+				//get the user id
 				contractList = contractService.getFinalizedContract(userId);
-				// Save contractList to request
+				//send contract list
 				request.setAttribute("contractList", contractList);
-				// Forward to page of contract to be approved
+				//turn to jsp
 				request.getRequestDispatcher("/FinalizedContract.jsp").forward(request, response);
-			} catch (AppException e) {
+			} catch (AppException e)
+			{
 				e.printStackTrace();
-				//Redirect to the exception page
 				response.sendRedirect("toError");
 			}
 		}
 	}
 	
-	/**
-	 * Process GET requests
-	 */
-	public void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		// Call doPost() to process request
+	public void doGet(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException 
+	{
 		this.doPost(request, response);
 	}
 
