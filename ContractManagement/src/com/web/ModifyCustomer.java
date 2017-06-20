fdb1ad68459515ad9df41e16093a96f538c38aaa
afrahao <15301043@bjtu.edu.cn>
2017-06-21 00:14:38 +0800

    Update ModifyCustomer.java

--- ContractManagement/src/com/web/ModifyCustomer.java
+++ ContractManagement/src/com/web/ModifyCustomer.java
@@ -1,38 +1,35 @@
 package com.web;
 
-import java.io.IOException;
+import java.io.*;
+import java.text.*;
+import java.util.*;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+import javax.servlet.*;
+import javax.servlet.http.*;
 
 import com.dao.impl.CustomerDaoImpl;
-import com.dao.impl.RoleDaoImpl;
-import com.model.Customer;
-import com.model.Role;
+import com.model.*;
+import com.service.*;
+import com.utils.*;
 
 public class ModifyCustomer extends HttpServlet{
 
-	/**
-	 * Jump to page of user permission list
-	 */
-	public void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {	
-		// Set character set of request to "UTF-8"
+	public void doPost(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException 
+	{
 		request.setCharacterEncoding("UTF-8");
-		
-		// Declare session
 		HttpSession session = null;
-		// Get session by using request
 		session = request.getSession();
+		//get user id
 		Integer userId = (Integer)session.getAttribute("userId");
 		CustomerDaoImpl customerdao=new CustomerDaoImpl();
-		// If user is not login, jump to login page
-		if (userId == null) {
+		//let the user login if not
+		if (userId == null) 
+		{
 			response.sendRedirect("toLogin");
-		}else {
+		}
+		else
+		{
+			//get and set customer information
 			Customer customer=new Customer();
 			customer.setAccout(request.getParameter("account"));
 			customer.setAddress(request.getParameter("address"));
@@ -44,17 +41,16 @@ public class ModifyCustomer extends HttpServlet{
 			customer.setName(request.getParameter("name"));
 			customer.setNum(request.getParameter("num"));
 			customer.setTel(request.getParameter("tel"));
+			//write customer information to database
 			customerdao.modifyCustomer(customer);
+			//turn to jsp
 			response.sendRedirect("customerList.jsp");
 		}
 	}
 	
-	/**
-	 * Process GET requests
-	 */
-	public void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		// Call doPost() to process request
+	
+	public void doGet(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException 
+	{
 		this.doPost(request, response);
 	}
 
