d1bfe722bc69fa253ff799a6b0bcc9fb313878c3
afrahao <15301043@bjtu.edu.cn>
2017-06-21 00:13:53 +0800

    Update LogoutServlet.java

--- ContractManagement/src/com/web/LogoutServlet.java
+++ ContractManagement/src/com/web/LogoutServlet.java
@@ -1,44 +1,26 @@
 package com.web;
 
-import java.io.IOException;
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
-/**
- * Servlet that process user logout
- */
 public class LogoutServlet extends HttpServlet{
 
-	/**
-	 * Process POST logout request
-	 */
 	public void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// Declare session
 		HttpSession session = null;
-		// Get session by using request object
 		session = request.getSession();
-		// Delete use information from session
+		//get user id and name
 		session.removeAttribute("userId");
 		session.removeAttribute("userName");
-		// Redirect to login page
-<<<<<<< HEAD
+		//turn to login
 		response.sendRedirect("index");
-=======
-		response.sendRedirect("toLogin");
->>>>>>> origin/LiWenjie
 	}
 
-	/**
-	 * Process the GET requests
-	 */
+	
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// Call doPost() to process request
 		this.doPost(request, response);
 	}
 	
